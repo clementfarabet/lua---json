dc39bdde2e92b6640e10add9aa3aa18a83e4b64a
Clement Farabet <clement.farabet@gmail.com>
2012-06-25 14:50:26 -0400

    Fixed init with more info

--- init.lua
+++ init.lua
@@ -1,3 +1,26 @@
+-----------------------------------------------------------------------------
+-- TAKEN From JSON4Lua, originally written for Lua 5.1
+-- Wrapped for torch-pkg (Clement Farabet)
+--
+-- JSON4Lua: JSON encoding / decoding support for the Lua language.
+-- json Module.
+-- Author: Craig Mason-Jones
+-- Homepage: http://json.luaforge.net/
+-- Version: 0.9.40
+-- This module is released under the MIT License (MIT).
+
+-- USAGE:
+-- This module exposes 4 functions:
+--   json_string = encode(o)
+--     Returns the table / string / boolean / number / nil / json.null value as a JSON-encoded string.
+--   o = decode(json_string)
+--     Returns a Lua object populated with the data encoded in the JSON string json_string.
+--
+--   save(json_file, o)
+--     Saves the table / string / boolean / number / nil / json.null value as a JSON-encoded file.
+--   o = load(json_file)
+--     Returns a Lua object populated with the data encoded in the JSON file.
+-----------------------------------------------------------------------------
 
 require 'dok'
 
