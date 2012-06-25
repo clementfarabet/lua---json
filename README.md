949dc76936d088abd3fba56580dd89c02a853d68
madBITS <madBITS@la-madbits.myhome.westell.com>
2012-06-25 14:16:18 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# json: a package to handle json (read and write).
+
+## Install:
+
+First install Torch7 (www.torch.ch) then simply install this package
+using torch-pkg:
+
+```
+torch-pkg deploy
+```
+
+## Use:
+
+The library provides 2 high-level functions: json.load and json.save. To get help
+on these functions, simply do:
+
+```
+> help(json.save)
+> help(json.load)
+```
---
+++ init.lua
@@ -0,0 +1,36 @@
+
+require 'dok'
+
+json = {}
+
+torch.include('json', 'json.lua')
+
+function json.load(path)
+   local _, path = dok.unpack(
+      {...},
+      'json.load',
+      'Load a JSON file'
+      {arg='path', type='string', help='path to file', req=true}
+   )
+   path = path:gsub('^~',os.getenv('HOME'))
+   local f = io.open(path,'r')
+   local js = f:read('*all')
+   f:close()
+   local tb = json.decode(js)
+   return tb
+end
+
+function json.save(...)
+   local _, path, tb = dok.unpack(
+      {...},
+      'json.save',
+      'Save a table to a JSON file'
+      {arg='path', type='string', help='path to file', req=true},
+      {arg='table', type='table', help='table to save', req=true}
+   )
+   path = path:gsub('^~',os.getenv('HOME'))
+   local js = json.encode(tb)
+   local f = io.open(path,'w')
+   f:write(js)
+   f:close()
+end
